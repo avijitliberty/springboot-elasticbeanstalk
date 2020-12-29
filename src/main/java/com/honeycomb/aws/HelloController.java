739a077fed1df0c10193ea1baaf01375ac934749
avijitliberty <avijit.liberty@gmail.com>
2020-12-29 14:51:43 -0800

    Changed hello controller REST endpoint

--- src/main/java/com/honeycomb/aws/HelloController.java
+++ src/main/java/com/honeycomb/aws/HelloController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
 @RestController
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/")
     public String hello() {
         return "Hello World";
     }
